98bdeeb047c99a312f500c820cbaf32e224c211e
MenOfTool <a101013322@gmail.com>
2019-11-27 08:57:48 +0800

    Change structure

--- app.py
+++ app.py
@@ -2,56 +2,29 @@ from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from uuid import uuid4
 from util import merger_v2
+
 app = Flask(__name__)
 
 SAVE_PATH = 'static/images/'
-ALLOWED_MIME = ['image/jpeg', 'image/png']
-GO_BACK_LINK = '<a href="/">Go back</a>'
-
-@app.route('/api1', methods=['GET'])
-def api1():
-    return 'Hello world'
 
 @app.route('/')
 def index():
-    return render_template('upload.html')
+    return 'hello world'
 
-@app.route('/upload')
-def upload_file():
-    return render_template('upload.html')
+@app.route('/postdata', methods=['POST'])
+def submitFile():
+    data = request.json
 
-@app.route('/error')
-def error_page():
-    return render_template('error.html')
+    status = 'OK'
+    result_msg = 'Failed'
+    result = 'None'
 
-@app.route('/uploader', methods=['GET', 'POST'])
-def upload_files():
-    if request.method == 'POST':
-        mode = request.form['mode']
-        rows = int(request.form['rows'])
-        file_list = request.files.getlist('file')
-        
-        if mode == 'cs':
-            if rows <= 0:
-                return "<h3>Rows should bigger than 0!</h3><br>{}".format(GO_BACK_LINK)
-            elif rows > len(file_list):
-                return "<h3>Too many rows, sorry!</h3><br>{}".format(GO_BACK_LINK)
+    images = data['images']
+    mode = data['mode']
 
-        dst = uuid4().hex + '.jpg'
-        fs_n = []
-        for f in file_list:
-            if f.content_type not in ALLOWED_MIME:
-                return "<h3>Please upload png & jpg only!</h3><br>{}".format(GO_BACK_LINK)
+    result_msg, result = m.mergeImages(images, mode)
 
-            f.filename = SAVE_PATH + uuid4().hex + '.jpg'
-            f.save(f.filename)
-            fs_n.append(f.filename)
-
-        X = merger_v2.Merging(mode, fs_n, SAVE_PATH+dst, rows)
-        if X != 0:
-            return "<h3>Error occured!</h3><br>{}".format(GO_BACK_LINK)
-
-        return render_template('result.html', img=dst)
+    return {'status': status, 'result_msg': result_msg, 'result': result}
 
 if __name__ == '__main__':
     app.run(debug=True)
